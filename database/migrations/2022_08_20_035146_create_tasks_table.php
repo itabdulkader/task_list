65e00981a9d45a1a95662cc93514d80f82da08bf
itabdulkader <it.abdulkader@gmail.com>
2022-08-20 05:59:30 +0200

    db migrations

---
+++ database/migrations/2022_08_20_035146_create_tasks_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string("title",191)->nullable()->default("");
+            $table->text("description")->nullable();
+            $table->foreignId('assigned_by_id')->references('id')->on('admins')->cascadeOnDelete();
+            $table->foreignId('assigned_to_id')->references('id')->on('users')->cascadeOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
